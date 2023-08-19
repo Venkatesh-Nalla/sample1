1f58f4cb6330230d3dc1e168289f1a387ed42b05
KATRAVATH VINOD <katravathvinod01@gmail.com>
2023-08-19 11:45:23 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import Card from './components/Card.js';
+import {useState,useEffect} from 'react'
 function App() {
+  const [data,setData]=useState({});
+  useEffect(()=>{
+    axios.get('https://jsonmock.hackerrank.com/api/football_matches?page=120').then((res)=>{
+      setData(res.data);
+      console.log(res.data);
+    }).catch((err)=>{
+        console.log(err);
+    })
+    
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:"flex",backgroundColor:"lime"}}>
+       {data.data && data.data.map((com) => {
+        // <Card name="dfdsfdfd" year="2023"/>
+       return <Card name={com.competition} year={com.year}  />
+       })
+       }
+       
+       
     </div>
   );
 }
